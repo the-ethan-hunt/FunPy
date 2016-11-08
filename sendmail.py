c0bbd0ab76697c0d848230ab080ebfb9bc124108
Dhruv Apte <dhruvgirishapte@gmail.com>
2016-11-08 12:13:19 +0530

    Update sendmail.py
    code to sent a mail

--- sendmail.py
+++ sendmail.py
@@ -1,14 +1,13 @@
 # coding: utf-8
 from bs4 import BeautifulSoup
 import smtplib
-fromaddr='dhruvgirishapte@gmail.com'
-toaddrs='vermaparth97@gmail.com'
+fromaddr='sender's mail'
+toaddrs='ID where mail is to be sent'
 msg='Hi! i sent this message via Python. Peace!'
 server=smtplib.SMTP("smtp.gmail.com:587")
 server.starttls()
-server.login(dhruvgirishapte@gmail.com,iamgreat)
-username='dhruvgirishapte@gmail.com'
-password='iamgreat'
+username='ID'
+password='password'
 server.login(username,password)
 server.sendmail(fromaddrs,toaddrs,msg)
 server.sendmail(fromaddr,toaddrs,msg)
